5b9e38b2e48769c0909b3e707baa28ba6de70ab4
alb <tse38@mail.ru>
2021-03-08 22:51:29 +0300

    HW1 is completed

--- hw01_hello_otus/go.mod
+++ hw01_hello_otus/go.mod
@@ -1,3 +1,3 @@
-module github.com/fixme_my_friend/hw01_hello_otus
+module github.com/tse38/hw_test/hw01_hello_otus
 
 go 1.16
--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+// ver 08.03.2021 22:01
+import (
+	"fmt"
+	"github.com/tse38/hw_test/hw01_hello_otus/stringutil"
+)
+
 func main() {
 	// Place your code here.
+	fmt.Println(stringutil.Reverse("Hello, OTUS!"))
 }
